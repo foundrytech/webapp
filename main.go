2a7aa5b26b88d8f122dfb9684eec38178d408bb7
jicodes <liu.ji1@northeastern.edu>
2024-03-20 21:54:11 -0700

    extract checkHealthzHandler into handlers package

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 
 	"github.com/jicodes/webapp/handlers"
@@ -22,20 +19,7 @@ func setupRouter() *gin.Engine {
 	r.Use(middlewares.CheckRequestMethod())
 	r.Use(middlewares.CheckPayload())
 
-	r.GET("/healthz", func(c *gin.Context) {
-		c.Header("Cache-Control", "no-cache, no-store, must-revalidate")
-		c.Header("Pragma", "no-cache")
-		c.Header("X-Content-Type-Options", "nosniff")
-
-		if err := initializers.DB.Exec("SELECT 1").Error; err == nil {
-			c.Status(http.StatusOK)
-			log.Println("Health check passed: 200 OK, DB is up and running")
-		} else {
-			c.Status(http.StatusServiceUnavailable)
-			log.Fatal("Health check failed: 503 Service Unavailable, DB is down or not reachable")
-		}
-	})
-	
+	r.GET("/healthz", handlers.CheckHealthz)
 	r.POST("/v1/user", handlers.CreateUser)
 	r.GET("/v1/user/self", middlewares.BasicAuth(), handlers.GetUser)
 	r.PUT("/v1/user/self", middlewares.BasicAuth(), handlers.UpdateUser)
