4e9e975a4dfe125bb5f217ab26c5ceefba881199
jicodes <liu.ji1@northeastern.edu>
2024-03-20 21:47:26 -0700

    rename 'controllers' to 'handlers'

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/jicodes/webapp/controllers"
+
+	"github.com/jicodes/webapp/handlers"
 	"github.com/jicodes/webapp/initializers"
 	"github.com/jicodes/webapp/middlewares"
 )
@@ -27,14 +29,16 @@ func setupRouter() *gin.Engine {
 
 		if err := initializers.DB.Exec("SELECT 1").Error; err == nil {
 			c.Status(http.StatusOK)
+			log.Println("Health check passed: 200 OK, DB is up and running")
 		} else {
 			c.Status(http.StatusServiceUnavailable)
+			log.Fatal("Health check failed: 503 Service Unavailable, DB is down or not reachable")
 		}
 	})
 	
-	r.POST("/v1/user", controllers.CreateUser)
-	r.GET("/v1/user/self", middlewares.BasicAuth(), controllers.GetUser)
-	r.PUT("/v1/user/self", middlewares.BasicAuth(), controllers.UpdateUser)
+	r.POST("/v1/user", handlers.CreateUser)
+	r.GET("/v1/user/self", middlewares.BasicAuth(), handlers.GetUser)
+	r.PUT("/v1/user/self", middlewares.BasicAuth(), handlers.UpdateUser)
 
 	return r
 }
